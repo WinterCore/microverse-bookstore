3debd22cd508a72f963fc490b3521d0795147fa7
WinterCore <hogobbl@gmail.com>
2021-02-23 06:07:54 +0200

    Add the ability to remove a book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types';
 
-function Book({ id, title, category }) {
+function Book({
+  id,
+  title,
+  category,
+  handleRemoveBook,
+}) {
   return (
     <tr>
       <td>{ id }</td>
       <td>{ title }</td>
       <td>{ category }</td>
+      <td>
+        <button
+          onClick={() => handleRemoveBook({ id, title, category })}
+          type="button"
+        >
+          Remove
+        </button>
+      </td>
     </tr>
   );
 }
@@ -14,6 +27,7 @@ Book.propTypes = {
   id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import { books } from '../actions';
 
 import Book from '../components/Book';
 
-function BooksList({ books }) {
+function BooksList({ books, removeBook }) {
   return (
     <table>
       <thead>
@@ -16,7 +19,13 @@ function BooksList({ books }) {
       <tbody>
         {
           books.map(book => (
-            <Book key={book.id} id={book.id} title={book.title} category={book.category} />
+            <Book
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              category={book.category}
+              handleRemoveBook={removeBook}
+            />
           ))
         }
       </tbody>
@@ -30,8 +39,12 @@ BooksList.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   })).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({ books: state.books });
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ removeBook: books.removeBook }, dispatch)
+);
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
