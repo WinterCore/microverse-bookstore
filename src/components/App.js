a2103c91e97172e2d0a8cb17a4370440ccf0ce9b
WinterCore <hogobbl@gmail.com>
2021-02-23 14:08:59 +0200

    Style header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Header from '../containers/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <BooksList />
       <BooksForm />
     </div>
