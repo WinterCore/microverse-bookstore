d2722213435ee1beb08ab1a74f100f52855a84f6
WinterCore <hogobbl@gmail.com>
2021-02-23 05:41:40 +0200

    Add categories

---
+++ src/data.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+export const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+/* eslint-enable import/prefer-default-export */
