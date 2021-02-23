331aea2d19054eb46068cd5222b6be530197e5f6
WinterCore <hogobbl@gmail.com>
2021-02-23 05:42:27 +0200

    Add BooksList component

---
+++ src/containers/BooksList.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Book from '../components/Book';
+
+function BooksList({ books }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          books.map(book => (
+            <Book key={book.id} id={book.id} title={book.title} category={book.category} />
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+const mapStateToProps = state => ({ books: state.books });
+
+export default connect(mapStateToProps)(BooksList);
